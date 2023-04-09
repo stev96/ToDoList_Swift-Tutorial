da55d84d2b3192738827f83d9e1952fb2f246a0b
Steven <webdeploymentnf@gmail.com>
2023-04-09 17:42:29 +0200

    Navigation Link hinzugefügt	verstanden Nein

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -2,25 +2,92 @@
 //  ContentView.swift
 //  ToDoList
 //
-//  Created by steven rezac on 07.04.23.
+//  Created .
 //
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+struct Aufgabe: Identifiable {
+    
+    var id = UUID()
+    var bezeichnung: String
+    var erledigt = false
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ContentView: View {
+    
+    @State var isBold = false
+    @State  var abstand = 0
+    @State var neueAufgabe = ""
+    @State var aufgaben: [Aufgabe] = [
+    ]
+    
+    // Oberflächeninhalt
+    var body: some View {
+        NavigationView {
+            VStack {
+                List() {
+                    ForEach(aufgaben.indices, id: \.self) { i in
+                        HStack {
+                            Button(action: {
+                                aufgaben[i].erledigt.toggle()
+                                
+                            }, label: {
+                                if aufgaben[i].erledigt {
+                                    Image(systemName: "checkmark.circle.fill")
+                                        .foregroundColor(.green)
+                                } else {
+                                    Image(systemName: "circle")
+                                }
+                            }).buttonStyle(PlainButtonStyle())
+                            NavigationLink(destination: DetailView(bezeichnung: $aufgaben[i].bezeichnung)) {
+                                Text(aufgaben[i].bezeichnung)
+                            }
+                        }
+                    }
+                    .onDelete(perform: { offset in
+                        aufgaben.remove(atOffsets: offset)
+                    })
+                }
+                
+                Spacer()
+                HStack {
+                    TextField("Neue Aufgabe", text: $neueAufgabe)
+                    Button("Hinzufügen") {
+                        let aufgabe = Aufgabe(bezeichnung: neueAufgabe)
+                        aufgaben.append(aufgabe)
+                        neueAufgabe = ""
+                    }
+                }
+                .padding(.horizontal, 30)
+                }
+                .navigationTitle("Meine Aufgaben")
+            
+            }
+        }
     }
+    
+struct DetailView: View {
+    
+    @Binding var bezeichnung: String
+    
+    var body: some View {
+        TextField("Aufgabe", text: $bezeichnung)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+    
 }
